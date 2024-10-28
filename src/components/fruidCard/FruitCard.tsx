8a479e1940d4e936591d2ab62c7a780b2257c473
Yurii Kyrylov <ykyrylov@deloitte.com>
2024-10-28 03:36:19 -0600

    updates styles for fruit card

--- src/components/fruidCard/FruitCard.tsx
+++ src/components/fruidCard/FruitCard.tsx
@@ -1,7 +1,8 @@
 // FruitCard.tsx
 import React from 'react';
 import { Card, CardContent, Typography, Button } from '@mui/material';
-import { Fruit } from '../../types/fruit';
+import { Fruit } from 'types/fruit';
+import styles from './FruitCard.module.css';
 
 interface FruitCardProps {
   fruit: Fruit;
@@ -10,16 +11,9 @@ interface FruitCardProps {
 
 const FruitCard: React.FC<FruitCardProps> = ({ fruit, onAdd }) => {
   return (
-    <Card variant="outlined" style={{ margin: '4px 0' }}>
-      <CardContent
-        style={{
-          display: 'flex',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-          padding: '5px 10px',
-        }}
-      >
-        <div style={{ display: 'flex', flexDirection: 'column' }}>
+    <Card variant="outlined" className={styles.card}>
+      <CardContent className={styles.cardContent}>
+        <div className={styles.fruitInfo}>
           <Typography>{fruit.name}</Typography>
           <Typography variant="body2" color="textSecondary" style={{ fontSize: '12px' }}>
             ({fruit.nutritions.calories} calories)
@@ -28,10 +22,7 @@ const FruitCard: React.FC<FruitCardProps> = ({ fruit, onAdd }) => {
         <Button
           variant="contained"
           color="primary"
-          style={{
-            padding: '4px 8px',
-            fontSize: '10px',
-          }}
+          className={styles.addButton}
           onClick={() => onAdd(fruit)}
         >
           Add
