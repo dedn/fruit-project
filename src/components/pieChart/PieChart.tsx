28c1d03f1c16a2606dd2efd5d9fedd28e650a9c2
Yurii Kyrylov <ykyrylov@deloitte.com>
2024-10-28 03:05:04 -0600

    update charts component

--- src/components/pieChart/PieChart.tsx
+++ src/components/pieChart/PieChart.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { Pie } from 'react-chartjs-2';
 import { Fruit } from '../../types/fruit';
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
@@ -7,20 +7,45 @@ interface PieChartProps {
   jar: Fruit[];
 }
 
-ChartJS.register(ArcElement, Tooltip, Legend); // Registering the necessary elements
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 const PieChart: React.FC<PieChartProps> = ({ jar }) => {
-  const data = {
-    labels: jar.map((fruit) => fruit.name),
-    datasets: [
-      {
-        data: jar.map((fruit) => fruit.nutritions.calories),
-        backgroundColor: jar.map(() => `#${Math.floor(Math.random() * 16777215).toString(16)}`),
-      },
-    ],
+  const data:
+    | { datasets: { backgroundColor: string[]; data: number[] }[]; labels: string[] }
+    | {
+        datasets: { backgroundColor: string[]; data: number[]; hoverOffset: number }[];
+        labels: string[];
+      } = useMemo(() => {
+    if (jar.length === 0) {
+      return {
+        labels: ['No fruits available'],
+        datasets: [{ data: [1], backgroundColor: ['#ccc'] }],
+      };
+    }
+
+    const labels = jar.map((fruit) => fruit.name);
+    const caloriesData = jar.map((fruit) => fruit.nutritions.calories);
+
+    const backgroundColors = jar.map(() => `#${Math.floor(Math.random() * 16777215).toString(16)}`);
+
+    return {
+      labels,
+      datasets: [
+        {
+          data: caloriesData,
+          backgroundColor: backgroundColors,
+          hoverOffset: 4,
+        },
+      ],
+    };
+  }, [jar]);
+
+  const options = {
+    responsive: true,
+    maintainAspectRatio: true,
   };
 
-  return <Pie data={data} />;
+  return <Pie data={data} options={options} />;
 };
 
 export default PieChart;
